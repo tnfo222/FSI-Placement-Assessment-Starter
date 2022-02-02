9161c379810c12550b48964d97c2b4f49b027641
tnfo222 <github.com/tnfo222/psychic-disco>
2022-02-01 19:30:51 -0500

    Gave function to the plus and minus buttons; Count the total number of cookies in basket

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,102 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Taylor Forbes"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = 0 // Cookie Total
 
+//Functions for updating the display quantities of each cookie and the total number of cookies
+function gbUpdateQuan (displayQuantity){
+    let gb = document.querySelector('#qty-gb')
+    gb.innerHTML = displayQuantity
+}
+function ccUpdateQuan (displayQuantity){
+    let cc = document.querySelector('#qty-cc')
+    cc.innerHTML = displayQuantity
+}
+function sugarUpdateQuan (displayQuantity){
+    let sugar = document.querySelector('#qty-sugar')
+    sugar.innerHTML = displayQuantity
+}
+function totQuan (displayTotQuantity){
+    let total = document.querySelector('#qty-total')
+    total.innerHTML = displayTotQuantity
+}
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+
+// selecting the element with an id of add-gb, add-cc, and add-sugar
 const gbPlusBtn = document.querySelector('#add-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const gbMinBtn = document.querySelector('#minus-gb')
+const ccMinBtn = document.querySelector('#minus-cc')
+const sugarMinBtn = document.querySelector('#minus-sugar')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    gb++
+    total++
+    gbUpdateQuan(`${gb}`)
+    totQuan(`${total}`)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Event listener for clicks on the "-" button for Gingerbread cookies
+gbMinBtn.addEventListener('click', function() {
+    if (gb > 0) {
+        gb--
+        total--
+    }
+    else {
+        window.alert('You do not have Gingerbread cookies in your basket :(')
+    }
+    gbUpdateQuan(`${gb}`)
+    totQuan(`${total}`)
+})
+
+//Event listener for clicks on the "+" button for Chocolate Chip cookies
+ccPlusBtn.addEventListener('click', function() {
+    cc++
+    total++
+    ccUpdateQuan(`${cc}`)
+    totQuan(`${total}`)
+})
+
+// Event listener for clicks on the "-" button for Chocolate Chip cookies
+ccMinBtn.addEventListener('click', function() {
+    if (cc > 0) {
+        cc--
+        total--
+    }
+    else {
+        window.alert('You do not have Chocolate Chip cookies in your basket :(')
+    }
+    ccUpdateQuan(`${cc}`)
+    totQuan(`${total}`)
+})
+
+//Event listener for clicks on the "+" button for Sugar Sprinkle cookies
+sugarPlusBtn.addEventListener('click', function() {
+    sugar++
+    total++
+    sugarUpdateQuan(`${sugar}`)
+    totQuan(`${total}`)
+})
+
+// Event listener for clicks on the "-" button for Sugar Sprinkle cookies
+sugarMinBtn.addEventListener('click', function() {
+    if (sugar > 0) {
+        sugar--
+        total--
+    }
+    else {
+        window.alert('You do not have Sugar Sprinkle cookies in your basket :(')
+    }
+    sugarUpdateQuan(`${sugar}`)
+    totQuan(`${total}`)
+})
